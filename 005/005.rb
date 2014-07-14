1db892f41889db8ed506b95faa9b1ca02110455a
schinsue <sergio@mentorme.nl>
2014-07-15 00:04:58 +0200

    solution #5

--- 005/005.rb
+++ 005/005.rb
@@ -1,27 +1,2 @@
-require 'Prime'
-
-def prime_division_range (from, to)
-	array = []
-	(from..to).each do |i|
-		array << i.prime_division
-	end
-	array
-end
-
-def lcm (array)
-
-	sum = 0
-
-	array.collect do |i|
-		
-		unless i.empty?
-			i.flatten!
-			# sum *= ( ** 1 )
-			p i
-		end
-	end
-end
-
-test = prime_division_range(1,20)
-
-p lcm(test)
+# Solution 5
+p (1..20).inject(:lcm)
