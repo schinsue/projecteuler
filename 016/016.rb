4c28da9e4fcd7426a09cc70e106eee05da57e8e7
Sergio Chin-Sue <sergio.chin-sue@finalist.com>
2015-08-07 10:37:13 +0200

    Added Solution 16

---
+++ 016/016.rb
@@ -0,0 +1,13 @@
+# Solution 16
+
+# 2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2**1000?
+
+timer_start = Time.now
+
+number = 2 ** 1000
+sum = number.to_s.chars.map(&:to_i).inject(:+)
+
+puts "The sum of the digits that come out of the number 2**1000 is #{sum}"
+puts "Elapsed Time: #{(Time.now - timer_start)*1000} milliseconds"
